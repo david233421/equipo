ec014e30b53718f602e5a091e4911a98dc2618f9
Night3080 <aacj081019hgtljra3@cbtis217.edu.mx>
2024-10-29 08:33:39 -0600

    Termine una parte de la estructura del apartado de lengua y comunicacion

---
+++ equipo/Formelaborarensayo.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC40
+ * Date: 29/10/2024
+ * Time: 08:20 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace equipo
+{
+	/// <summary>
+	/// Description of Formelaborarensayo.
+	/// </summary>
+	public partial class Elaborarensayo : Form
+	{
+		public Elaborarensayo()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
